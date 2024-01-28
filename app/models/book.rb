3205ac8d130365825bf1cc8a7d0fb5ca13f6821b
Carsonwl <carson.long@gmail.com>
2024-01-28 16:50:25 -0600

    Standard CRUD

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+    validates :title, presence: true
+    validates :author, presence: true
+    validates :pages, presence: true
+    validates :cover, presence: true
 end
