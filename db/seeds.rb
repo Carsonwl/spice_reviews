4bae135dcb0db3c25e55aec9955685fd07b85e3c
Carsonwl <carson.long@gmail.com>
2024-01-28 15:14:48 -0600

    set up db and controller for Book

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# ActiveRecord::Schema[7.1].define(version: 2024_01_28_210415) do
+#     create_table "books", force: :cascade do |t|
+#       t.string "title"
+#       t.string "author"
+#       t.integer "pages"
+#       t.string "cover"
+#       t.datetime "created_at", null: false
+#       t.datetime "updated_at", null: false
+#     end
+  
+#   end
+
+potot = Book.create(title: "Priory of the Orange Tree", author: "Samantha Shannon", pages: 804, cover: "https://m.media-amazon.com/images/I/91jJM3OtsML._AC_UF1000,1000_QL80_.jpg")
+
